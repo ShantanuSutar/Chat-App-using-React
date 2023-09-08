6facba4e3b19f67302288ab44706d212508eadb7
Shantanu Sutar <shantanusutar17@gmail.com>
2023-09-09 01:41:28 +0530

    creating Navbar component - Logo, User and Logout

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,5 +1,20 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="  flex items-center bg-cyan-700 h-16 p-[10px] justify-between text-cyan-50">
+      <span className=" font-semibold text-lg">Chit Chat</span>
+      <div className="flex items-center gap-2">
+        <img
+          src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
+          alt="User Photo"
+          className=" bg-cyan-50 h-6 w-6 rounded-full object-cover"
+        />
+        <span>Sam</span>
+        <button className=" bg-cyan-600 text-cyan-50 text-sm border-none p-1 rounded-sm cursor-pointer hover:bg-cyan-500">
+          Logout
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- app/src/components/Sidebar.jsx
+++ app/src/components/Sidebar.jsx
@@ -1,5 +1,12 @@
+import Navbar from "./Navbar";
+import Search from "./Search";
+
 const Sidebar = () => {
-  return <div className=" flex-[1] bg-cyan-600">Sidebar</div>;
+  return (
+    <div className=" flex-[1] bg-cyan-600">
+      <Navbar />
+    </div>
+  );
 };
 
 export default Sidebar;
